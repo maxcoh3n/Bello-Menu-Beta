b507e91ca0384af6c98bfbf8554a5022b876b5f0
maxcoh3n <maxcoh3n@gmail.com>
2020-09-25 11:52:11 -0700

    insert new menu into db

--- addMenu2.php
+++ addMenu2.php
@@ -12,110 +12,34 @@ include 'common/bellodentrostarter.php'; ?>
 <h3>Menu Creation</h3>
 
 
-<form method="post" action="addMenu2.php">
+<form method="post" action="addMenu3.php">
 
 <?php
 
 $MenuName = $_POST["MenuName"];
-$TopText = $_POST["TopText"];
-$BottomText = $_POST["BottomText"];
-$BackText = $_POST["BackText"];
+$TopText = $cxn->real_escape_string($_POST["TopText"]);
+$BottomText = $cxn->real_escape_string($_POST["BottomText"]);
+$BackText = $cxn->real_escape_string($_POST["BackText"]);
 
-	echo        
-		"<table cellspacing='10'>
-		<addadishtd colspan='30'></td></tr>
-		<tr>
-		</tr>
-		";	
 
-	echo 
-	"<tr>
-	<td>Course</td>
-	<td>  
-        <select name = 'CourseCategory' required='required'>        
-                <option value='Antipasti'>Antipasti</option>
-                <option value='Pasta'>Pasta</option>
-                <option value='Secondi'>Secondi</option>
-                <option value='Contorni'>Contorni</option>
-        </select>
-    </td>
-    </tr>
 
-	<tr>
-	<td>Dish Name</td>
-	<td><input type='text' name='DishName' id='DishName' required='required' placeholder='required'></td>
-	</tr>
 
-	<tr>
-	<td>Ingredient 1</td>
-	<td><input type='text' name='Ingredient1' id='Ingredient1' required='required' placeholder='required'></td>
-	</tr>
+$query = "INSERT INTO `Menus` (MenuName, TopText, BottomText, BackText)
 
-	<tr>
-	<td>Ingredient 2</td>
-	<td><input type='text' name='Ingredient2' id='Ingredient2' placeholder='optional'></td>
-	</tr>
+VALUES
+	(
+		'$MenuName',
+		'$TopText',
+		'$BottomText',
+		'$BackText'
+	)
+";
 
-	<tr>
-	<td>Ingredient 3</td>
-	<td><input type='text' name='Ingredient3' id='Ingredient3' placeholder='optional'></td>
-	</tr>
-
-	<tr>
-	<td>Ingredient 4</td>
-	<td><input type='text' name='Ingredient4' id='Ingredient4' placeholder='optional'></td>
-	</tr>
-
-	<tr>
-	<td>Ingredient 5</td>
-	<td><input type='text' name='Ingredient5' id='Ingredient5' placeholder='optional'></td>
-	</tr>
-
-	<tr>
-	<td>Ingredient 6</td>
-	<td><input type='text' name='Ingredient6' id='Ingredient6' placeholder='optional'></td>
-	</tr>
-
-	<tr>
-	<td>Price</td>
-	<td><input type='Number' step='0.01' name='Price' id='Price' required='required' placeholder='required'></td>
-	</tr>
-
-	<tr>
-	<td>Gluten Free</td>
-	<td><input type='checkbox' name='GlutenFree' id='GlutenFree'></td>
-	</tr>
-
-	<tr>
-	<td>Vegan</td>
-	<td><input type='checkbox' name='Vegan' id='Vegan'></td>
-	</tr>
-
-	<tr>
-	<td>Raw Ingredients</td>
-	<td><input type='checkbox' name='Raw' id='Raw'></td>
-	</tr>
-
-	<tr>
-	<td>Spicy</td>
-	<td><input type='checkbox' name='Raw' id='Raw'></td>
-	</tr>
-
-	<tr>
-	<td>Active (on the Current Menu)</td>
-	<td><input type='checkbox' name='Active' id='Active' checked></td>
-	</tr> 
-
-	<tr>
-	<td><input type='submit' value=' Submit ' name='submit'/></td>
-	</tr>
-	";
-
-echo "</table>";
-echo "</form>";
-
+// echo $query;
+$result1= mysqli_query($cxn,$query)
+			or die ("Database query to insert new Menu did not work, please contact I.T.");
+			echo "$MenuName added to database\n";
 
 ?>
-
 </body>
 </html>
