c9e8ddc7d46c969e71c0bea269988abb3157e467
maxcoh3n <maxcoh3n@gmail.com>
2020-09-26 16:06:09 -0700

    added default to getCourses func

--- common/courses.php
+++ common/courses.php
@@ -7,25 +7,38 @@ the array returned has CourseName for reference and ExtendedName for printing th
 function getCourses($MenuID){
   include 'databaseConnection.php';
 
+  if($MenuID != ""){
+
     $MenuOrder = "Menu" . $MenuID . "Order";
 
-  $query= "SELECT CourseName, ExtendedName FROM Courses WHERE '$MenuOrder' IS NOT NULL ORDER BY '$MenuOrder'" ;
-  $result= mysqli_query($cxn,$query) or die ("Database query to get Courses didn't work. Please contact IT.");;
-  if ($cxn->connect_error) {
-    die("Connection failed: " . $cxn->connect_error);
-}
+    $query= "SELECT CourseName, ExtendedName FROM Courses WHERE '$MenuOrder' IS NOT NULL ORDER BY '$MenuOrder'" ;
+    $result= mysqli_query($cxn,$query) or die ("Database query to get Courses didn't work. Please contact IT.");;
+    if ($cxn->connect_error) {
+      die("Connection failed: " . $cxn->connect_error);
+    }
 
-  $CourseArray = array();
-
-  while($row = mysqli_fetch_assoc($result))
-  {
-      array_push($CourseArray, array(
-          "CourseName" => $row['CourseName'],
-          "ExtendedName" => $row['ExtendedName']
-      ));
+  }else{
+    $query= "SELECT CourseName, ExtendedName FROM Courses" ;
+    $result= mysqli_query($cxn,$query) or die ("Database query to get Courses didn't work. Please contact IT.");;
+    if ($cxn->connect_error) {
+      die("Connection failed: " . $cxn->connect_error);
+    }  
   }
+
+    $CourseArray = array();
+
+    while($row = mysqli_fetch_assoc($result))
+    {
+        array_push($CourseArray, array(
+            "CourseName" => $row['CourseName'],
+            "ExtendedName" => $row['ExtendedName']
+        ));
+    }
+
 
   return $CourseArray;
 }
+
+
 
 ?>
