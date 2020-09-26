fb93d337f4bba01aba91d235da9c17b30f4352a2
maxcoh3n <maxcoh3n@gmail.com>
2020-09-25 18:27:49 -0700

    edited getCourses function to base off  new col

--- common/courses.php
+++ common/courses.php
@@ -1,13 +1,31 @@
 <?php
 
-// IN future, possibly replace with sql statement
-function getCourses($Menu){
-    return array(
-        'Antipasti', 
-        'Pasta',
-        'Secondi',
-        'Contorni',
-    );
+
+/* this function returns the courses, in order, associated with the given menu. 
+the array returned has CourseName for reference and ExtendedName for printing the menu
+*/
+function getCourses($MenuID){
+  include 'databaseConnection.php';
+
+    $MenuOrder = "Menu" . $MenuID . "Order";
+
+  $query= "SELECT CourseName, ExtendedName FROM Courses WHERE '$MenuOrder' IS NOT NULL ORDER BY '$MenuOrder'" ;
+  $result= mysqli_query($cxn,$query) or die ("Database query to get Courses didn't work. Please contact IT.");;
+  if ($cxn->connect_error) {
+    die("Connection failed: " . $cxn->connect_error);
+}
+
+  $CourseArray = array();
+
+  while($row = mysqli_fetch_assoc($result))
+  {
+      array_push($CourseArray, array(
+          "CourseName" => $row['CourseName'],
+          "ExtendedName" => $row['ExtendedName']
+      ));
+  }
+
+  return $CourseArray;
 }
 
 ?>
