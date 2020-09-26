f727169e30ba8a86023b32d5019c68be8b2c1fda
maxcoh3n <maxcoh3n@gmail.com>
2020-09-26 10:23:07 -0700

    tweaks to delete Menu pages

---
+++ deleteMenu.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+  <title>Delete The Menu</title>
+  </head>
+  <body>
+
+  <?php 
+  include 'common/auth.php';
+  include 'common/bellodentrostarter.php'; 
+ 
+  ?>
+
+  <h2> Delete Menu Page 1</h2>
+
+
+  <form method="post" action="deleteMenu2.php">
+
+
+    <?php 
+    $query= "SELECT * FROM Menus ORDER BY MenuID" ;
+    //print $query;
+    $result= mysqli_query($cxn,$query);
+    if ($cxn->connect_error) {
+      die("Connection failed: " . $cxn->connect_error);
+    }
+    
+    $MenuArray = array();
+
+      while($row = mysqli_fetch_assoc($result))
+      {
+        array_push($MenuArray,array(
+            'MenuName' => $row['MenuName'],
+            'MenuID' => $row['MenuID']
+        ));
+      }
+
+    echo "Menu: <select name = 'MenuID|MenuName' required='required' >  ";
+    for($ct = 0; $ct < count($MenuArray); $ct++){
+        $MenuName = $MenuArray[$ct]['MenuName'];
+        $MenuID = $MenuArray[$ct]['MenuID'];
+        echo "<option value= '$MenuID|$MenuName' > $MenuName </option>";
+    }
+    
+    
+    ?>
+
+  <input type="submit" value="DELETE" style="background-color: #f44336; font-weight:bold">  
+  </form>
+
+  To add a new Menu, click <a href="addMenu.php" target='_blank'>here</a>
+  <br>
+
+  To edit an existing Menu, click <a href="editMenu.php" target='_blank'>here</a>
---
+++ deleteMenu2.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>Delete Menu 2</title>
+</head>
+<body>
+
+
+<?php  include 'common/bellodentrostarter.php';
+
+echo "<h3>Menu Deletion</h3>";
+            
+
+$result = $_POST["MenuID|MenuName"];
+$result_explode = explode('|', $result);
+$MenuID = $result_explode[0];
+$MenuName = $result_explode[1];
+
+//DELETE ACTUAL MENU----------------------------------------------------------
+$query1 = "DELETE FROM `Menus` WHERE `MenuID` = $MenuID";
+$result1= mysqli_query($cxn,$query1)
+			or die ("Database query1 to delete menu did not work, please contact I.T.");
+
+$MenuActive = "Menu" . $MenuID . "Active";
+
+//DELETE COLUMN FROM DISHSE----------------------------------------------------------
+$query2 = "ALTER TABLE `Dishes` DROP `$MenuActive`";
+$result2= mysqli_query($cxn,$query2)
+			or die ("Database query2 to delete menu from dishes did not work, please contact I.T.");
+
+$MenuOrder = "Menu" . $MenuID . "Order";
+
+//DELETE COLUMN FROM COURSES----------------------------------------------------------
+$query3 = "ALTER TABLE `Courses` DROP `$MenuOrder`";
+$result3= mysqli_query($cxn,$query3)
+			or die ("Database query3 to delete menu from courses did not work, please contact I.T.");
+
+
+echo $MenuName . " Successfully deleted";
+
+
+?>
+</body>
+</html>
